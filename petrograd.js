381347b8311e0fba087b0de706f9d75f82a5f3ff
Ann Sophie Fedder Lindskjold <AnnSophieLindskjold@Anns-MacBook-Pro-3.local>
2017-02-25 18:22:54 +0100

    Each category row, Javascript changes if else filters

--- petrograd.js
+++ petrograd.js
@@ -2,40 +2,18 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
-
-
     // læs produktliste
-    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
-
-    document.querySelector(".filterknap_vegetar").addEventListener("click", filtrerVegetar);
-
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?",
+        visProduktListe);
 }
-
-function filtrerVegetar(event) {
-    console.log("klik på vegetar-filter");
-
-    //find alle ikke-vegetar-produkter
-    var liste = document.querySelectorAll(".produkt:not(.vegetar)");
-
-    //skjul dem - tilføj klassen hide
-    liste.forEach(div => produkt.classList.toggle("hide"));
-    event.preventDefault();
-}
-
 
 function visProduktListe(listen) {
     console.table(listen);
-    // filtrer udsolgte produkter fra
-    listen = listen.filter(produkt => !produkt.udsolgt);
-
     listen.forEach(visProdukt);
 }
 
-
-
-
-
 function visProdukt(produkt) {
+
     console.log(produkt);
     // klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
@@ -65,41 +43,63 @@ function visProdukt(produkt) {
         rabatpris.parentNode.removeChild(rabatpris);
     }
 
-    //tilføj produkt-id til modalknap
+
+    //if (produkt.vegetar == true) {
+    //    klon.querySelector(".produkt").classList.add("vegetar");
+    //}
+
+    // tilføj produkt-id til modalknap
     klon.querySelector(".modalknap").dataset.produkt = produkt.id;
 
     // registrer klik på modalknap
     klon.querySelector(".modalknap").addEventListener("click", modalKnapKlik)
-
+        // append klon til .produkt_liste
+        //document.querySelector(".produktliste").appendChild(klon);
+        //}
 
 
     // append klon til .produkt_liste
-    document.querySelector(".produktliste").appendChild(klon);
+
+    if (produkt.kategori == "forretter") {
+        document.querySelector(".forretliste").appendChild(klon);
+    } else if (produkt.kategori == "hovedretter") {
+        document.querySelector(".hovedretliste").appendChild(klon);
+    } else if (produkt.kategori == "desserter") {
+        document.querySelector(".dessertliste").appendChild(klon);
+    } else if (produkt.kategori == "drikkevarer") {
+        document.querySelector(".drikkevarerliste").appendChild(klon);
+    } else if (produkt.kategori == "sideorders") {
+        document.querySelector(".sideordersliste").appendChild(klon);
+    }
+
 }
+
 
 function modalKnapKlik(event) {
     console.log("knapklik", event);
-    find det produkt id, hvis knap der blev trykket på
+
+    // find det produkt id, hvis knap der blev trykket på
     var produktId = event.target.dataset.produkt;
     console.log("Klik på produkt: ", produktId);
+
     $.getJSON("http://petlatkea.dk/2017/dui/api/product?callback=?", {
         id: produktId
     }, visModalProdukt);
-
 }
 
 function visModalProdukt(produkt) {
-    console.log("hvis modal for", produkt);
-    find modal_template - klon den
-    var klon = document.querySelector("#modal_template").content.cloneNode(true);
+    console.log("vis modal for ", produkt);
 
-    //put data i klonen
+    // find modal_template - klon den
+    var klon =
+        document.querySelector("#modal_template").content.cloneNode(true);
 
-    //sletter det der stod i modal-content
+    // put data i klonen
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+
+    // sletter det der stod i modal-content
     document.querySelector(".modal-content").innerHTML = "";
 
-    append klonen til modal content
+    // append klonen til modal-content
     document.querySelector(".modal-content").appendChild(klon);
-
-
 }
